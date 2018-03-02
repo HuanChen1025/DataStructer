d8b85588cbe84183bf7f84ce7eb06c90cca2e970
xiaohuan1025@gmail.com <huanchen1025@163.com>
2018-03-02 20:21:40 +0800

    3.2

---
+++ src/lintCode/Reverse_Array.java
@@ -0,0 +1,20 @@
+package lintCode;
+
+public class Reverse_Array {
+    /**
+     * Reverse the given array nums inplace.
+     * @param nums: a integer array
+     * @return: nothing
+     */
+    public void reverseArray(int[] nums) {
+        // write your code here
+        int temp = nums[nums.length-1];
+        for (int i = nums.length-1;i>0;i--){
+//            nums[i]
+
+
+        }
+
+
+    }
+}
--- src/lintCode/Valid_Perfect_Square.java
+++ src/lintCode/Valid_Perfect_Square.java
@@ -4,14 +4,29 @@ public class Valid_Perfect_Square {
     /**
      * @param num: a positive integer
      * @return: if num is a perfect square else False
+     *  Example
+        For example:
+        Given num = 16
+        Returns True
      */
-    public boolean isPerfectSquare(int num) {
+    public static boolean isPerfectSquare(int num) {
         // write your code
-
-
-
-
-
+        if(num == 1){
+            return true;
+        }
+        long x = num/2;
+        long t = x * x; //改为long类型，防止num值过大时计算溢出
+        while (t > num){
+            x = x/2;
+            t = x*x;
+        }
+        for (long i = x; i <= 2*x ;++i){
+            if ( i*i == num ) return true;
+        }
         return false;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(isPerfectSquare(1000000));
     }
 }
