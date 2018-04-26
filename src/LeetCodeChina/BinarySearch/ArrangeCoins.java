dfd161ea5f0b39d7a09680472b81f65f95a1cd82
xiaohuan1025@gmail.com <huanchen1025@163.com>
2018-04-26 19:49:12 +0800

    2018.4.26

---
+++ src/LeetCodeChina/BinarySearch/ArrangeCoins.java
@@ -0,0 +1,40 @@
+package LeetCodeChina.BinarySearch;
+
+public class ArrangeCoins {
+
+    /**
+     * 采用二分查找的思路
+     *
+     * 还有一种数学解法
+     * @param n
+     * @return
+     */
+    public int arrangeCoins(int n) {
+        if (n<=1)
+            return 1;
+
+        int start =0;
+        int end =n;
+
+        while (start <= end){
+            int middle = start +(end-start)/2;
+
+            int sum = middle*(middle+1)/2;
+
+            if (sum>n){
+                end = middle-1;
+            }
+            else if (sum<=n){
+                start = middle+1;
+            }
+
+        }
+        return start-1;
+    }
+
+    public static void main(String[] args) {
+        ArrangeCoins obj = new ArrangeCoins();
+        System.out.println(obj.arrangeCoins(6));
+
+    }
+}
