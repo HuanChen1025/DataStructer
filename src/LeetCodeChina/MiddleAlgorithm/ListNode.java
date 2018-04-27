01b9fa14522457c59b24024194de27517a43b8ee
xiaohuan1025@gmail.com <huanchen1025@163.com>
2018-04-27 22:04:03 +0800

    change repo

--- src/LeetCodeChina/MiddleAlgorithm/ListNode.java
+++
@@ -1,7 +0,0 @@
-package LeetCodeChina.MiddleAlgorithm;
-
-public class ListNode {
-      public int val;
-      public   ListNode next;
-     public   ListNode(int x) { val = x; }
-}
--- src/LeetCodeChina/PrimaryAlgorithm/List/ListNode.java
+++
@@ -1,8 +0,0 @@
-package LeetCodeChina.PrimaryAlgorithm.List;
-
-public class ListNode {
-    int val;
-    ListNode next;
-    ListNode(int x) { val = x; }
-
-}
